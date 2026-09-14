b063c020990913db1f23531305ee1cb737982b7d
Avishkar Yelave <113693113+Avishkarbot@users.noreply.github.com>
2026-09-14 10:24:32 +0530

    1 to n Without Loops

---
+++ 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void printTillN(int n) {
+        if(n!=1){
+             printTillN(n-1);
+        }
+      
+       System.out.print(n+" ");
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
