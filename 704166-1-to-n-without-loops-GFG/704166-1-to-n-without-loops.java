0797978142dc47e7ef91a6bec66bf65d15a0c4a7
Avishkar Yelave <113693113+Avishkarbot@users.noreply.github.com>
2026-09-14 10:25:42 +0530

    Update 704166-1-to-n-without-loops.java

--- 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.java
+++ 704166-1-to-n-without-loops-GFG/704166-1-to-n-without-loops.java
@@ -7,7 +7,3 @@ class Solution {
        System.out.print(n+" ");
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
